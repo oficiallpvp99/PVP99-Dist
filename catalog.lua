762a924ee7ffb7fa0cd2cda3b7400eb4900574c2
oficiallpvp99 <oficialpvp99@gmail.com>
2026-09-26 05:28:01 -0300

    Add files via upload

---
+++ catalog.lua
@@ -0,0 +1,22 @@
+-- PVP99 - CATALOGO REMOTO
+-- Para adicionar um novo bot ao painel, basta adicionar uma nova entrada.
+
+return {
+  {
+    name = "BOOK WORLD",
+    folder = "Book-World",
+    version = "1.0",
+    installer = "Book-World/installer.lua",
+    enabled = true
+  }
+
+  -- Exemplo futuro:
+  -- ,
+  -- {
+  --   name = "ROSHAMUUL",
+  --   folder = "Roshamuul",
+  --   version = "1.0",
+  --   installer = "Roshamuul/installer.lua",
+  --   enabled = true
+  -- }
+}
